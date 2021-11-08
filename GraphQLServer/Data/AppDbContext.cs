4ce2168102adf4a5d36c8eb3675c7e74e8172ad4
Stakan27 <Stakan27@list.ru>
2021-11-08 23:00:53 +0500

    add models

---
+++ GraphQLServer/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GraphQLServer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLServer.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Catalog> Catalogs { get; set; }
+        public DbSet<SubCatalog> SubCatalogs { get; set; }
+    }
+}
---
+++ GraphQLServer/Models/Catalog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLServer.Models
+{
+    public class Catalog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+    }
+}
